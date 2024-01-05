26618a580122dc5d35d26b7ecb9fb7c31c4fabde
Ivan Yurlov <ivanyurlov@yandex.ru>
2024-01-05 19:33:40 +0300

    feat: 3 редакция

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+//import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
@@ -9,9 +10,11 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    {/*<BrowserRouter>*/}
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    {/*</BrowserRouter>*/}
+    </HashRouter>
   </React.StrictMode>
 );
 
